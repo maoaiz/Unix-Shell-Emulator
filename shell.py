4bc442cdf59008c02c3c1c7ad8bfd8e45580540d
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-03-14 11:01:51 -0500

    Simple login users

--- shell.py
+++ shell.py
@@ -1,9 +1,18 @@
 # -*- coding: cp1252 -*-
 import getpass
+USERS_FILE_DIR = "users.txt"
+
 
 def get_user(u, p):
     """Buscar en el archivo"""
-    return True
+    f = open(USERS_FILE_DIR)
+    for line in f.readlines():
+        l = line.strip('\n').split(" ")
+        if l[0] == u and l[1] == p:
+            f.close()
+            return True
+    f.close()
+    return False
 
 def get_command(c):
     """Buscar en el archivo"""
@@ -40,7 +49,7 @@ def main():
                 print "Welcome", user, "\n"
                 init_shell(user)
         else:
-            print "Invalid password for", user, "\n"
+            print "Login incorrect."
     exit()
 
 
